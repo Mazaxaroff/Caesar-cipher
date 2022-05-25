9554fe7ee9088c45ed36dc8053ee89d3477429f2
Mazaxaroff <mzxrov@mail.ru>
2022-05-25 23:22:53 +0300

    Изменен метод absolutePathOfFile. Добавлены методы alfabet, сipher и decipher.

--- src/ru/javarush/maxzaharov/ceasarcipher/Main.java
+++ src/ru/javarush/maxzaharov/ceasarcipher/Main.java
@@ -8,6 +8,8 @@ import java.util.*;
 
 public class Main {
     private static int key;
+    private static final int sizeOfAlfabet = alfabet().size();
+    private static final List<Character> ALFABET = alfabet();
 
     public static void main(String[] args) {
         dialog();
@@ -22,14 +24,14 @@ public class Main {
                 enterKey();
                 do {
                     key = keyFrom();
-                } while (key == 0);
-                for (String s : listFromFile(absolutePathOfFile())) {
-                    System.out.println(s);
+                } while (key == 0 || Math.abs(key) > sizeOfAlfabet);
+                for (String s : listFromFile(pathOfFile())) {
+                    System.out.println(cihper(s, key));
                 }
 
 
             } else {
-                System.out.println("Ключ будет сгенерирован случайным образом");
+                System.out.println("Ключ будет сгенерирован случайным образом из диапазона возможных");
                 key = keyRandom();
 
             }
@@ -41,7 +43,10 @@ public class Main {
                     enterKey();
                     do {
                         key = keyFrom();
-                    } while (key == 0);
+                    } while (key == 0 || Math.abs(key) > sizeOfAlfabet);
+                    for (String s : listFromFile(pathOfFile())) {
+                        System.out.println(decihper(s, key));
+                    }
 
                 } else { //brutForse
 
@@ -79,7 +84,7 @@ public class Main {
     }
 
     public static void enterKey() {
-        System.out.println("Пожалуйста введите целое число от 1 до 32");
+        System.out.println("Пожалуйста введите целое число от 1 до " + sizeOfAlfabet);
     }
 
     public static int keyFrom() {
@@ -89,6 +94,8 @@ public class Main {
             keyFrom = scanner.nextInt();
             if (keyFrom == 0) {
                 System.out.println("Вы ввели ноль! Попробуйте еще раз!");
+            } else if (Math.abs(keyFrom) > sizeOfAlfabet) {
+                System.out.println("Вы ввели число, выходящее из диапазона ключей! Попробуйте еще раз!");
             }
         } catch (InputMismatchException e) {
             System.err.println("Введено не целое число! Попробуйте еще раз!");
@@ -99,32 +106,29 @@ public class Main {
     public static int keyRandom() {
         int keyRandom;
         Random random = new Random();
-        keyRandom = random.nextInt(33);
+        keyRandom = random.nextInt(sizeOfAlfabet);
         return keyRandom;
     }
 
-    public static Path absolutePathOfFile() {
+    public static Path pathOfFile() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Пожалуйста укажите путь к файлу, из которого требуется обработать текст");
-        String pathOfFile;
+        System.out.println("Пожалуйста введите полный путь к файлу, из которого требуется обработать текст " +
+                "на русском языке");
+        String path;
         do {
-            pathOfFile = scanner.nextLine();
+            path = scanner.nextLine();
             try {
-                Path absolutePath = Path.of(pathOfFile).toAbsolutePath().normalize();
-                if (!Files.exists(absolutePath)) {
+                Path pathOfFile = Path.of(path);
+                if (!Files.exists(pathOfFile)) {
                     System.out.println("Такого файла не существует, попробуйте еще раз!");
+                } else if (!Files.isRegularFile(pathOfFile)) {
+                    System.out.println("Введена директория, введите полный путь к файлу! ");
                 }
-                System.out.println(absolutePath);
-                System.out.println(Files.exists(absolutePath));
             } catch (InvalidPathException e) {
                 System.err.println("Путь не может быть сконвертирован");
-                
-            } catch (SecurityException e) {
-                System.err.println("Ошибка безопасности");
             }
-        } while (!Files.exists(Path.of(pathOfFile).toAbsolutePath().normalize()));
-        Path absolutePathOfFile = Path.of(pathOfFile).toAbsolutePath().normalize();
-        return absolutePathOfFile;
+        } while (!Files.exists(Path.of(path)));
+        return Path.of(path);
     }
 
     public static List<String> listFromFile(Path path) {
@@ -133,7 +137,7 @@ public class Main {
             try {
                 listFromFile = Files.readAllLines(path);
             } catch (IOException e) {
-                System.out.println("Произошла ошибка чтения файла");
+                System.err.println("Произошла ошибка чтения файла");
             }
         } else {
             System.out.println("Невозможно прочитать файл");
@@ -141,5 +145,50 @@ public class Main {
         return listFromFile;
     }
 
+    public static List<Character> alfabet() {
+        List<Character> alfabet = new ArrayList<>();
+        for (char i = 'А'; i <= 'я'; i++) {
+            alfabet.add(i);
+        }
+        alfabet.add('.');
+        alfabet.add(',');
+        alfabet.add('"');
+        alfabet.add(':');
+        alfabet.add(';');
+        alfabet.add('-');
+        alfabet.add('!');
+        alfabet.add('?');
+        alfabet.add(')');
+        alfabet.add('(');
+        alfabet.add(' ');
+        return alfabet;
+    }
 
+    public static String cihper(String s, int shift) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ALFABET.contains(ch)) {
+                char newChar = ALFABET.get((ALFABET.indexOf(ch) + shift) % sizeOfAlfabet);
+                stringBuilder.append(newChar);
+            } else {
+                stringBuilder.append(ch);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String decihper(String s, int shift) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ALFABET.contains(ch)) {
+                char newChar = ALFABET.get((ALFABET.indexOf(ch) +(sizeOfAlfabet-shift)) % sizeOfAlfabet);
+                stringBuilder.append(newChar);
+            } else {
+                stringBuilder.append(ch);
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
+
+
